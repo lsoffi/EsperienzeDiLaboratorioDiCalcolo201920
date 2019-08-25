b93eb9dd76f286d9b127c0d9f3cf1f9bd920befb
Cristiano De Michele <cristiano.demichele@gmail.com>
2019-08-25 14:14:59 +0200

    * upd

--- esercitazioni/ex4.py
+++ esercitazioni/ex4.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
 import numpy as np
-plt.title('Un primo plot con Python')
+plt.title('Esercizio 4')
 plt.xlabel('x')
 plt.ylabel('y')
-x = np.linspace(0.0, 5.0, 100)
-y = x
-plt.plot(x,y,label='y=x')
-n_bins=100
-x, y = np.loadtxt('temp.dat', usecols=(0,2), delimiter=' ', unpack=True)
-
-fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
-
-# We can set the number of bins with the `bins` kwarg
-axs[0].hist(x, bins=n_bins)
-axs[1].hist(y, bins=n_bins)
-
-plt.plot(x,y, 'x',label='Loaded from file!')
-plt.savefig('traiettoria.png')
+n_bins=36
+x, y = np.loadtxt('dati.dat', usecols=(0,1), delimiter=' ', unpack=True)
+plt.bar(x,y,fill=True)
+plt.savefig('isto1.png')
 #plt.clf()
 plt.show()
