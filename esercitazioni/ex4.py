12b0a068d8faa5ba103dd5f4cfe09630d21fdcb4
Cristiano De Michele <cristiano.demichele@gmail.com>
2019-08-25 12:19:37 +0200

    upd

--- esercitazioni/ex4.py
+++ esercitazioni/ex4.py
@@ -7,7 +7,15 @@ plt.ylabel('y')
 x = np.linspace(0.0, 5.0, 100)
 y = x
 plt.plot(x,y,label='y=x')
+n_bins=100
 x, y = np.loadtxt('temp.dat', usecols=(0,2), delimiter=' ', unpack=True)
+
+fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
+
+# We can set the number of bins with the `bins` kwarg
+axs[0].hist(x, bins=n_bins)
+axs[1].hist(y, bins=n_bins)
+
 plt.plot(x,y, 'x',label='Loaded from file!')
 plt.savefig('traiettoria.png')
 #plt.clf()
