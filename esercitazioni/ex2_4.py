7d0dd0f77c33a2322f773f8cbf66e690dd211a42
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-09-02 21:43:09 +0200

    * plot delle frecce senza zip ma ancora graficamente vanno migliorate

--- esercitazioni/ex2_4.py
+++ esercitazioni/ex2_4.py
@@ -6,18 +6,18 @@ plt.xlabel('x')
 plt.ylabel('y')
 #list of tuples with two elements (i.e. x and y coordinates)
 x, y, vx, vy = np.loadtxt('temp.dat', usecols=(0,1,2,3), unpack=True)
-arrows=zip(x,y,vx,vy)
 # le unità di misura delle frecce sono le stesse del plot
 # arrow(x,y,dx,dy) disegna una freccia da (x,y) a (x+dx,y+dy)
-for p in arrows:
-    xi=p[0]
-    yi=p[1]
-    vxi=p[2]
-    vyi=p[3]
-    plt.arrow(xi,yi,vxi,vyi,width=0.01,head_width=0.1, head_length=3.2, fc='r', ec='r')
+for cc in range(0,len(x)):
+    xi=x[cc]
+    yi=y[cc]
+    vxi=vx[cc]
+    vyi=vy[cc]
+    print ('cc='+str(cc))
+    plt.arrow(xi,yi,5*vxi,5*vyi,width=0.2,head_width=0.5,head_length=0.3, fc='r', ec='r')
 plt.plot(x,y,label='traiettora con velocità')
 #ec=colore del bordo fc=colore di riempimento (e.g. r=red b=blue etc.)
-plt.plot(x,y, 'x',label='Loaded from file!')
+#plt.plot(x,y, 'x',label='Loaded from file!')
 plt.savefig('traiettoria.png')
 #plt.clf()
 plt.show()
