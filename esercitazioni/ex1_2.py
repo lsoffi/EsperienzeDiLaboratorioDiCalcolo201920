e5722fc69c24dd6aa6c542c12bb578b84613c38c
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-09-02 18:58:06 +0200

    modificati i testi di ex1 e ex2 e anche gli script relativi

--- esercitazioni/ex1_2.py
+++ esercitazioni/ex1_2.py
@@ -4,6 +4,6 @@ import numpy as np
 plt.title('Un primo plot con Python')
 plt.xlabel('Tc')
 plt.ylabel('Tf')
-x, y = np.loadtxt('temp.dat', delimiter=' ', unpack=True)
+x, y = np.loadtxt('temp.dat', unpack=True)
 plt.plot(x,y, 'x',label='Loaded from file!')
 plt.show()
