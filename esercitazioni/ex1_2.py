9ce8ef47411c610fb569ef672555a2bcaf921fcf
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-09-02 11:06:42 +0200

    esercizio 1 punto 3 ora completo rispetto al .tex

--- esercitazioni/ex1_2.py
+++ esercitazioni/ex1_2.py
@@ -2,11 +2,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 plt.title('Un primo plot con Python')
-plt.xlabel('x')
-plt.ylabel('y')
-x = np.linspace(0.0, 5.0, 100)
-y = x
-plt.plot(x,y,label='y=x')
+plt.xlabel('Tc')
+plt.ylabel('Tf')
 x, y = np.loadtxt('temp.dat', delimiter=' ', unpack=True)
 plt.plot(x,y, 'x',label='Loaded from file!')
 plt.show()
--- esercitazioni/ex1_3.py
+++ esercitazioni/ex1_3.py
@@ -4,9 +4,16 @@ import numpy as np
 plt.title('Un primo plot con Python')
 plt.xlabel('x')
 plt.ylabel('y')
-x = np.linspace(0.0, 5.0, 100)
-y = x
-plt.plot(x,y,label='y=x')
+x = np.linspace(-10.0, 50.0, 100)
+#y = x
+conv = 5.0/9.0
+offset = 32.0
+y=x/conv + offset
+plt.plot(x,y,label='retta interpolante')
 x, y = np.loadtxt('temp.dat', delimiter=' ', unpack=True)
-plt.plot(x,y, 'x',label='Loaded from file!')
+plt.plot(x,y, 'x',label='temp.dat')
+#ottiene gli estremi attuali
+#left, right = plt.xlim()  
+plt.xlim(left=0)
+plt.xlim(right=50.0)
 plt.show()
