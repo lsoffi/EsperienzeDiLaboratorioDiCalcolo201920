ed4ee86b0d7aa515961742b6589ab6431de6665c
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-09-03 12:16:31 +0200

    * finito esercizio 2 (tex e py)

--- esercitazioni/ex2_4.py
+++ esercitazioni/ex2_4.py
@@ -8,16 +8,17 @@ plt.ylabel('y')
 x, y, vx, vy = np.loadtxt('temp.dat', usecols=(0,1,2,3), unpack=True)
 # le unità di misura delle frecce sono le stesse del plot
 # arrow(x,y,dx,dy) disegna una freccia da (x,y) a (x+dx,y+dy)
+# anche width, head_width, etc. sono in unità di misura del plot
+# di conseguenza se gli assi hanno unità differenti le frecce risultano "deformate"
 for cc in range(0,len(x)):
     xi=x[cc]
     yi=y[cc]
     vxi=vx[cc]
     vyi=vy[cc]
-    print ('cc='+str(cc))
-    plt.arrow(xi,yi,5*vxi,5*vyi,width=0.2,head_width=0.5,head_length=0.3, fc='r', ec='r')
+    plt.arrow(xi,yi,20*vxi,20*vyi,width=0.05,head_width=0.4,head_length=0.4, fc='r', ec='r')
+ax=plt.gca() # gca = get current axes
+#stesse unità di misura per gli assi x e y per non deformare le frecce
+ax.set_aspect('equal')
 plt.plot(x,y,label='traiettora con velocità')
-#ec=colore del bordo fc=colore di riempimento (e.g. r=red b=blue etc.)
-#plt.plot(x,y, 'x',label='Loaded from file!')
 plt.savefig('traiettoria.png')
-#plt.clf()
 plt.show()
