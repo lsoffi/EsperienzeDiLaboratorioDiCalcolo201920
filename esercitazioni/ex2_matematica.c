4941c12fa8b9f9564e3bafb971bbc89b93cbf3ac
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-09-04 19:39:52 +0200

    * ex2 c code

--- esercitazioni/ex2_matematica.c
+++ esercitazioni/ex2_matematica.c
@@ -3,8 +3,8 @@
 #include<math.h>
 int main(int argc, char **argv)
 {
-  printf("8+7=%d 3.2+8.4=%G 2^2+4^2=%G sqrt(36)=%.7G\n", 8+7, 3.2+8.4, pow(2,2)+pow(4,2), sqrt(36));
-  printf("cos(pi/6)=%.7G sin(pi/6)=%.7G tanh(1)=%.7G sinh(0)=%.7G\n", cos(M_PI/6), sin(M_PI/6), tanh(1), sinh(0));
+  printf("8+7=%3d [%%3d]\t\t3.2+8.4=%.4f [%%.4f]\t2^2+4^2=%.4f [%%.4f]\tsqrt(36)=%.4f [%%.4f]\n", 8+7, 3.2+8.4, pow(2,2)+pow(4,2), sqrt(36));
+  printf("\ncos(pi/6)=%.4f[%%.4f]\tsin(pi/6)=%.4f[%%.4f]\ttanh(1)=%.4f [%%.4f]\tsinh(0)=%.4f [%%.4f]\n", cos(M_PI/6), sin(M_PI/6), tanh(1), sinh(0));
   /* la precision con F indica il numero di cifre dopo la virgola con G il massimo numero di cifre signficative
    * dal comando:
    *
@@ -17,7 +17,8 @@ int main(int argc, char **argv)
    * are removed from the fractional part of the result; a decimal point appears only if it is
    * followed by at least one digit.
    */
-  printf("cos(pi/6)+10=%.7F cos(pi/6)+10=%.7G \n", cos(M_PI/6)+10., cos(M_PI/6)+10.);
+  printf("\ncos(pi/6)+10=%.7F [%%.7F]\t\t\tcos(pi/6)+10=%.7G [%%.7G]\n", cos(M_PI/6)+10., cos(M_PI/6)+10.);
+  printf("\ncos(pi/6)*10^8=%.7G [%%.7G]\t\tcos(pi/6)/10^3=%.7G [%%.7G]\n",cos(M_PI/6)*100000000,cos(M_PI/6.0)/10000);
   return 0;// dice al sistema operativo che non ci sono errori
 }
 
