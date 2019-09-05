9874c1ec3aed35270c62098590065d21533f4bf2
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-09-05 13:43:44 +0200

    * aggiunta printf in ex2_matematica.c

--- esercitazioni/ex2_matematica.c
+++ esercitazioni/ex2_matematica.c
@@ -3,7 +3,8 @@
 #include<math.h>
 int main(int argc, char **argv)
 {
-  printf("8+7=%3d [%%3d]\t\t3.2+8.4=%.4f [%%.4f]\t2^2+4^2=%.4f [%%.4f]\tsqrt(36)=%.4f [%%.4f]\n", 8+7, 3.2+8.4, pow(2,2)+pow(4,2), sqrt(36));
+  printf("Benvenuto! Questo è un programma dimostrativo delle funzioni della libreria math.h.\n");
+  printf("\n8+7=%3d [%%3d]\t\t3.2+8.4=%.4f [%%.4f]\t2^2+4^2=%.4f [%%.4f]\tsqrt(36)=%.4f [%%.4f]\n", 8+7, 3.2+8.4, pow(2,2)+pow(4,2), sqrt(36));
   printf("\ncos(pi/6)=%.4f[%%.4f]\tsin(pi/6)=%.4f[%%.4f]\ttanh(1)=%.4f [%%.4f]\tsinh(0)=%.4f [%%.4f]\n", cos(M_PI/6), sin(M_PI/6), tanh(1), sinh(0));
   /* la precision con F indica il numero di cifre dopo la virgola con G il massimo numero di cifre signficative
    * dal comando:
---
+++ esercitazioni/ex2_motocircolare.c
@@ -0,0 +1,9 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+int main(int argc, char **argv)
+{
+
+  return 0;// dice al sistema operativo che non ci sono errori
+}
+
