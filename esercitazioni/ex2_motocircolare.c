cbda0ed338e92e7d3d51e441765decc5232bc53c
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-09-06 10:05:59 +0200

    * ex2_motocircolare.c checking

--- esercitazioni/ex2_motocircolare.c
+++ esercitazioni/ex2_motocircolare.c
@@ -3,7 +3,32 @@
 #include<math.h>
 int main(int argc, char **argv)
 {
-
+  double R, w, t, x, y;
+  R=6.2; /* m */
+  w=0.1; /* rad/s */
+  t = 0.0; /* s */
+  x = R*cos(w*t);
+  y = R*sin(w*t);
+  printf("t=%.4f x=%.5f y=%.5f\n", t, x, y);
+  t = 0.5; /* s */
+  x = R*cos(w*t);
+  y = R*sin(w*t);
+  printf("t=%.4f x=%.5f y=%.5f\n", t, x, y);
+  t = 10; /* s */
+  x = R*cos(w*t);
+  y = R*sin(w*t);
+  printf("t=%.4f x=%.5f y=%.5f\n", t, x, y);
+  t = 10; /* s */
+  x = R*cos(w*t);
+  y = R*sin(w*t);
+  printf("t=%.4f x=%.5f y=%.5f\n", t, x, y);
+  /* blocco interattivo */
+  printf("Immetti il tempo t:");
+  scanf("%lf", &t);
+  x = R*cos(w*t);
+  y = R*sin(w*t);
+  printf("t=%.4f x=%.5f y=%.5f\n", t, x, y);
+ 
   return 0;// dice al sistema operativo che non ci sono errori
 }
 
