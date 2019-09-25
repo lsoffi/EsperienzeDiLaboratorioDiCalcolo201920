7599797afdcc6a6a035f793e4aadbe2cde924a6c
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-09-25 09:16:46 +0200

    ciao_mondo.s funziona ora anche con gnu gcc

--- C_EXAMPLES/CAPITOLO_2/ciao_mondo.s
+++ C_EXAMPLES/CAPITOLO_2/ciao_mondo.s
@@ -9,14 +9,14 @@
 #
 # ----------------------------------------------------------------------------------------
  
-       .data 
+        .data 
 
 #ifdef LINUX_OS
-  #define write_syscall $60
-  #define exit_syscall  $1
+        write_syscall= 60
+        exit_syscall= 1
 #else
-  #define write_syscall $0x2000004
-  #define exit_syscall  $0x2000001
+        write_syscall= 0x2000004
+        exit_syscall= 0x2000001
 #endif
 
 message:
@@ -28,17 +28,25 @@ message:
 
 _main:
         # write(1, message, len)
+        #
         # mov -> 16 bit; movl -> 32 bit; movq -> 64 bit
-        # mov addr, reg: copia il valore corrispondente all'indirizzo addr nel registro reg
-        # leaq addr, reg: copia l'indirizzo addr nel registro reg
-        movl    write_syscall, %eax     # chiamata di sistema 0x20000004 ($1 in linux) ovvero scrivi su schermo
+        #
+        # mov label, reg:       copia il valore corrispondente all'indirizzo di 'label' nel registro reg
+        #
+        # mov $const, reg:      copia la costante 'const' nel registro 'reg'
+        #
+        # mov $label, reg:      copia l'indirizzo della variabile 'label' in 'reg'
+        #
+        # leaq label, reg:      copia l'indirizzo di label nel registro reg
+        #
+        movl    $write_syscall, %eax    # chiamata di sistema 0x20000004 ($1 in linux) ovvero scrivi su schermo
         movl    $1, %edi                # 1 è standard out (stdout)
         leaq    message(%rip), %rsi     # metto nel registro %rsi l'indirizzo della stringa da stampare
-        movq    $len, %rdx              # metto in %rdx il numero di bytes da stampare (lungh. stringa + 1)
+        movq    $len, %rdx              # metto in %rdx il numero di bytes da stampare (lungh. stringa + 1), len è una costante!
         syscall                         # effettua la chiamata di sistema
 
         # exit(0)
         movl     $0, %edi               # metto il codice da ritornare alla shell nel registro $edi
-        movl     exit_syscall, %eax       # metto in %eax il valore 0x2000001 ($60 in linux) ovvero exit()
+        movl     $exit_syscall, %eax       # metto in %eax il valore 0x2000001 ($60 in linux) ovvero exit()
         syscall                         # effettua la chiamata di sistema
 
