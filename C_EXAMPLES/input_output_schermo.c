2ed3f04177d14f32e39434e8e43814eed92e1173
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-09-25 14:24:07 +0200

    esempio cap3 modificato

--- C_EXAMPLES/input_output_schermo.c
+++ C_EXAMPLES/input_output_schermo.c
@@ -1,17 +1,45 @@
-#include<stdio.h>
+#include<stdio.h> // dichiarazioni di printf e scanf
+
+// definizione nel codice della macro, ma possono
+// anche essere definite dalla riga di comando in fase
+// di compilazione
+
+#define STAMPA_SEP 0 // oppure da riga di comando gcc -D STIMA_SEP=1 (o 0)
+#define CHIEDI_INTERO 
+// oppure #undef CHIEDI_INTERO oppure 
+// da riga di comando: gcc -D CHIEDI_INTERO 
+
+// definisco la macro K (valore da assegnare ad a, vedi sotto)
+#define K 2/3
+
 int main(int argc, char **argv)
 {
-  double a = 2/3, c=3000000000;
+  double a = K, c=3000000000;
   float b = 1.0/3; // casting implicito
   int i=5, l=-5;
   printf("a=%f b=%f  c=%g\n", a, b, c); 
-  printf("i=%d l=%d l=%ud\n", i, l, l); // casting implicito da int ad unsigned int
+  printf("i=%d l=%d l=%ud\n", i, l, l); // casting implicito da int ad unsigned int --> C è "weakly typed"!!
 
+#if STAMPA_SEP==1
+  printf("---------------------\n");
+#endif
+#if STAMPA_SEP>1
+  printf("*********************\n");
+#endif
   printf("Immetti un double:");
   scanf("%lf", &a);
   printf("Hai immesso a=%f\n", a);
+#ifdef CHIEDI_INTERO // o anche #if defined(CHIEDI_INTERO)
   printf("Immetti un intero:");
   scanf("%d", &i);
   printf("Hai immesso i=%d\n", i); 
+#endif
+
+#if 1
+  printf("fine1\n");
+#else
+  printf("fine2\n");
+#endif
+
   return 0;
 }
