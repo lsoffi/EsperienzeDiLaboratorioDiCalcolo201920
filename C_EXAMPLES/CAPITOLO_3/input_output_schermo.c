f5a3d674b0015c04d0e79050bf0002ad9e0b1671
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2019-09-26 09:50:57 +0200

    aggiunti operatori logici a input_output_schermo.c

--- C_EXAMPLES/CAPITOLO_3/input_output_schermo.c
+++ C_EXAMPLES/CAPITOLO_3/input_output_schermo.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
 {
   double a = K, c=3000000000;
   float b = 1.0/3; // casting implicito
-  int i=5, l=-5;
+  int i=5, l=-5, k;
   printf("a=%f b=%f  c=%g\n", a, b, c); 
   printf("i=%d l=%d l=%ud\n", i, l, l); // casting implicito da int ad unsigned int --> C è "weakly typed"!!
 
@@ -41,5 +41,13 @@ int main(int argc, char **argv)
   printf("fine2\n");
 #endif
 
+  // gli operatori relazionali (>, <, !=, ==) agiscono su interi o float e restituiscono vero (1) o falso (0) 
+  k = i > l;
+  printf("k=%d\tNOT k=%d\n", k, !k);
+
+
+  printf("k=3\t!k=%d\n", !3); // diverso da 0 vuol dire "vero", uguale a 0 vuol dire "falso"
+  
+  printf("(%d > %d) && 1 = %d\n", i, l, (i > l) && 1);
   return 0;
 }
